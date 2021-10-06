20986d01a66200c88f0beb4c78c97b82eda12fa3
한진 <201780089@jei.com>
2021-10-06 13:36:43 +0900

    newUpdate

---
+++ src/components/TodoHead.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import { useTodoState } from "../TodoContext";
+
+const TodoHeadBlock = styled.div`
+  padding-top: 48px;
+  padding-left: 48px;
+  padding-right: 48px;
+  padding-bottom: 48px;
+  border-bottom: 1px solid #e9ecef;
+
+  h1 {
+    margin: 0;
+    font-size: 36px;
+    color: #343a40;
+  }
+  .day {
+    margin-top: 4px;
+    color: #868e96;
+    font-size: 21px;
+  }
+  .tasks-left {
+    margin-top: 40px;
+    color: #20c997;
+    fornt-size: 18px;
+    font-weight: bold;
+  }
+`;
+
+function TodoHead() {
+  const todos = useTodoState();
+  const undoneTasks = todos.filter((todo) => !todo.done);
+
+  const today = new Date();
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const dayName = today.toLocaleDateString("ko-KR", {
+    weekday: "long",
+  });
+
+  return (
+    <TodoHeadBlock>
+      <h1>{dateString}</h1>
+      <div className="day">{dayName}</div>
+      <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
+    </TodoHeadBlock>
+  );
+}
+
+export default TodoHead;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,86 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { MdDone, MdDelete } from "react-icons/md";
+import { useTodoDispatch } from "../TodoContext";
+
+const Remove = styled.div`
+  opacity: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+  /*display: none;*/
+`;
+
+const TodoItemBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  &:hover {
+    ${Remove} {
+      opacity: 1;
+      /*display: initial;*/
+    }
+  } /* Component Selector 라는 기능 : TodoItemBlock 위에 커서가 있을 때, Remove 컴포넌트를 보여주라는 의미 */
+`;
+const CheckCircle = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  border: 1px solid #ced4da;
+  font-size: 24px; /* 아이콘 크기 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+  ${(props) =>
+    props.done &&
+    css`
+      border: 1px solid #38d9a9;
+      color: #38d9a9;
+    `}
+`;
+const Text = styled.div`
+  flex: 1; /* 자신이 차지할 수 있는 영역을 전체로 설정*/
+  font-size: 21px;
+  color: #495057;
+  ${(props) =>
+    props.done &&
+    css`
+      color: #ced4da;
+    `}
+`;
+
+function TodoItem({ id, done, text }) {
+  const dispatch = useTodoDispatch();
+  const onToggle = () =>
+    dispatch({
+      type: "TOGGLE",
+      id,
+    });
+  const onRemove = () =>
+    dispatch({
+      type: "REMOVE",
+      id,
+    });
+  return (
+    <TodoItemBlock>
+      <CheckCircle done={done} onClick={onToggle}>
+        {done && <MdDone />}
+      </CheckCircle>
+      <Text done={done}>{text}</Text>
+      <Remove onClick={onRemove}>
+        <MdDelete />
+      </Remove>
+    </TodoItemBlock>
+  );
+}
+
+export default React.memo(TodoItem); //컴퍼넌트 최적화
